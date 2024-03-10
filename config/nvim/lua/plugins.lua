4224671868a691a8992da3479cb4b3b623c9405c
Siddharth Singh <siddharths067@gmail.com>
2024-03-10 18:54:58 +0100

    Added initial lsp config

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -39,6 +39,18 @@ return require('packer').startup(function(use)
     -- NOTE: PUT YOUR THIRD PLUGIN HERE --
     ---------------------------------------
 
+    use { 'williamboman/mason.nvim' }
+    use { 'williamboman/mason-lspconfig.nvim'}
+    use { 'neovim/nvim-lspconfig' }            -- previous installed
+    use { 'hrsh7th/nvim-cmp', config = [[require('config.nvim-cmp')]] }    
+    use { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' } 
+    use { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' }        -- buffer auto-completion
+    use { 'hrsh7th/cmp-path', after = 'nvim-cmp' }          -- path auto-completion
+    use { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' }       -- cmdline auto-completion
+    use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
+
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
