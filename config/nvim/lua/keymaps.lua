5171dfd334c0f25ec6b5152430d970ec302f3b35
Siddharth Singh <siddharths067@gmail.com>
2024-03-16 19:03:46 +0100

    Added custom intial config with telescope and lazy.nvim

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,6 +1,28 @@
--- define common options
-local opts = {
-    noremap = true,      -- non-recursive
-    silent = true,       -- do not show message
-}
+local wk = require("which-key")
+
+wk.register({
+	e = {
+		name = 'Editor',
+		t = {
+			name = 'Theme',
+			l = {"<cmd>lua vim.o.background='light'<cr>", 'set light background'},
+			d = {"<cmd>lua vim.o.background='dark'<cr>", 'set dark background'},
+			c = {"<cmd>Telescope colorscheme<cr>", 'Choose a theme'}
+			
+		}
+	},
+	f = {
+		name = 'Find',
+		f = {
+			"<cmd>Telescope find_files<cr>", 'Find files'
+		}
+	},
+	o = {
+		name = 'Open',
+		r = {
+			"<cmd>Telescope oldfiles<cr>", 'Recent files'
+		}
+	}
+}, {prefix="<leader>"})
+
 
