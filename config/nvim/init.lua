44c84ee50c3103041ee9bad8a1072ff25a55b60e
Siddharth Singh <siddharths067@gmail.com>
2024-03-16 19:03:46 +0100

    Added custom intial config with telescope and lazy.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,20 @@
-require('options')
-require('keymaps')
-require('plugins')
-require('colorscheme')
-require('lsp')
+-- Initialise Lazy.nvim for package management
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({import = 'plugins'})
+require("keymaps")
+
+
+vim.cmd[[colorscheme koehler]]
 
