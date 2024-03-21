5b8785bf95090f8014366f395fb5ca5f8fca5f5f
Siddharth Singh <siddharths067@gmail.com>
2024-03-21 19:18:51 +0100

    Added configuration for lsp and editor

---
+++ config/nvim/lua/editor.lua
@@ -0,0 +1,10 @@
+-- Set theme
+vim.cmd[[colorscheme tokyonight-night]]
+
+
+
+-- show line numbers
+vim.opt.number = true 
+-- show horizontal line in editor
+vim.opt.cursorline = true 
+
--- config/nvim/lua/plugins/no_config_plugins.lua
+++ config/nvim/lua/plugins/no_config_plugins.lua
@@ -1,7 +1,12 @@
 return {
-    {
+	{
 	    'nvim-telescope/telescope.nvim', tag = '0.1.6',
 	    dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+	},
+	{
+	    "williamboman/mason.nvim",
+	    "williamboman/mason-lspconfig.nvim",
+	    "neovim/nvim-lspconfig",
+	}
 }
 
