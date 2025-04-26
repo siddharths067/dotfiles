6595b5d6ebb2bb6b031dc08cbc08c7761bd9a040
Siddharth Singh <siddharths067@gmail.com>
2025-04-26 21:25:40 +0200

    Added Oh-My-Zsh installation and git config

--- setup_config.sh
+++ setup_config.sh
@@ -2,3 +2,15 @@ set -e
 set -x
 echo "Stowing Neovim configuration"
 stow  -t $HOME/.config --dotfiles config -vv
+
+echo "Installing Oh-My-Zsh"
+
+if [ -n $ZSH ]; then
+	echo "ZSH Already installed";
+else
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+fi
+
+ln -sf $PWD/zsh_custom.zsh $ZSH/custom/zsh_custom.zsh 
+
+
